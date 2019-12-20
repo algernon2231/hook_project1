579dc6395cedcad76144cff1151894fe3d4e02e7
algernon2231 <39697959+algernon2231@users.noreply.github.com>
2019-12-21 06:32:24 +0700

    change1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import  Routes from './node_modules/routes';
-import { BrowserRouter as Router } from 'react-router-dom'
-import Topbar from './node_modules/components/topbar'
+import React from "react";
+import Routes from "./node_modules/routes";
+import { BrowserRouter as Router } from "react-router-dom";
+import Topbar from "./node_modules/components/topbar";
+import { CurrentUserProvider } from "./node_modules/contexts/currentUser";
+import  CurrentUserChecker from './node_modules/components/currentUserChecker'
 const App = () => {
-  return ( 
-      <Router>
+  return (
+    <CurrentUserProvider>
+      <CurrentUserChecker>
+        <Router>
           <Topbar />
           <Routes />
-          
+        </Router>
+      </CurrentUserChecker>
+    </CurrentUserProvider>
+  );
+};
 
-       </Router>
-  ) 
-}
-
-export default App
+export default App;
